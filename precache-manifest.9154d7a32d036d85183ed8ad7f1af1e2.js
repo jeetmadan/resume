503571f9ffcda62ee075c908fb03eaafafc05c2a
jeetmadan <kumarmadanjeet1997@gmail.com>
2020-05-11 08:23:47 +0530

    Updates

---
+++ precache-manifest.9154d7a32d036d85183ed8ad7f1af1e2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745a0d265a40a97a147ce9254888c15e",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "e469d61c8eeae8bdb522",
+    "url": "/resume/static/css/2.3433bf6e.chunk.css"
+  },
+  {
+    "revision": "3e51e2bb92fbc4dec2a8",
+    "url": "/resume/static/css/main.103f2369.chunk.css"
+  },
+  {
+    "revision": "e469d61c8eeae8bdb522",
+    "url": "/resume/static/js/2.5cbc9884.chunk.js"
+  },
+  {
+    "revision": "b32588be65181b268343cac9cfd20c32",
+    "url": "/resume/static/js/2.5cbc9884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e51e2bb92fbc4dec2a8",
+    "url": "/resume/static/js/main.7e3defa8.chunk.js"
+  },
+  {
+    "revision": "fc6ccfdad72e7b5d0278",
+    "url": "/resume/static/js/runtime-main.4beab0ad.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/resume/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/resume/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/resume/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/resume/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "eab9224b540296eab2adbfd353f46f75",
+    "url": "/resume/static/media/circle-cropped.eab9224b.png"
+  },
+  {
+    "revision": "36b557be8fabe82605cc061d67fb1d63",
+    "url": "/resume/static/media/madanjeet_final_cv.36b557be.pdf"
+  }
+]);
